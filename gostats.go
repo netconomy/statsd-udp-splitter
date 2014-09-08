615679b38148c47b55df5ebbcd672ff641733f26
Robert Bartl <robert@edv-bartl.at>
2014-09-08 16:21:39 +0200

    goopt included

--- gostats.go
+++ gostats.go
@@ -2,13 +2,25 @@ package main
 
 import (
   "net"
-
+  "strconv"
   "fmt"
-  collectd "github.com/paulhammond/gocollectd"
+//  collectd "github.com/paulhammond/gocollectd"
+  goopt "github.com/droundy/goopt"
 )
 
+
+var port = goopt.Int([]string{"-p", "--port"}, 8126, "UDP Port to use")
+
 func main() {
-  addr, _ := net.ResolveUDPAddr("udp", ":2000")
+  goopt.Description = func() string {
+		return "Metric Wrapper for (at first) graphite & elasticsearch."
+  }
+  goopt.Version = "1.0"
+  goopt.Summary = "gostats"
+  goopt.Parse(nil)
+
+
+  addr, _ := net.ResolveUDPAddr("udp", ":" + strconv.Itoa(*port))
   sock, _ := net.ListenUDP("udp", addr)
 
   i := 0
